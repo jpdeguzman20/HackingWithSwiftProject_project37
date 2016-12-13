261e86ef447e371728365fc6ec39adb1ebb1c253
JP Deguzman <jddeguzman@ucdavis.edu>
2016-12-12 21:18:19 -0800

    First commit.

---
+++ Project37/CardViewController.swift
@@ -0,0 +1,62 @@
+//
+//  CardViewController.swift
+//  Project37
+//
+//  Created by Jonathan Deguzman on 12/8/16.
+//  Copyright © 2016 Jonathan Deguzman. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    weak var delegate: ViewController!
+    
+    var front: UIImageView!
+    var back: UIImageView!
+    
+    var isCorrect = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.bounds = CGRect(x: 0, y: 0, width: 100, height: 140)
+        front = UIImageView(image: UIImage(named: "cardBack"))
+        back = UIImageView(image: UIImage(named: "cardBack"))
+        
+        view.addSubview(front)
+        view.addSubview(back)
+        
+        front.isHidden = true
+        back.alpha = 0
+        
+        UIView.animate(withDuration: 0.2) {
+            self.back.alpha = 1
+        }
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(cardTapped))
+        back.isUserInteractionEnabled = true
+        back.addGestureRecognizer(tap)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func cardTapped() {
+        delegate.cardTapped(self)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Project37/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Project37
+//
+//  Created by Jonathan Deguzman on 12/8/16.
+//  Copyright © 2016 Jonathan Deguzman. All rights reserved.
+//
+
+import UIKit
+import GameplayKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var cardContainer: UIView!
+
+    var allCards = [CardViewController]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadCards()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /// loadCards() assembles an array of positions where the cards need to go, loads the various zener shapes, and creates one card view controller for each position.
+    /// Returns: nil
+    /// Parameters: none
+    
+    func loadCards() {
+        // Remove any existing cards by looping through the card view controllers stored in the allCards array, remove the view, remove the view controller containment, and then clear the whole array.
+        for card in allCards {
+            card.view.removeFromSuperview()
+            card.removeFromParentViewController()
+        }
+        
+        allCards.removeAll(keepingCapacity: true)
+        
+        let positions = [
+            CGPoint(x: 75, y: 85),
+            CGPoint(x: 185, y: 85),
+            CGPoint(x: 295, y: 85),
+            CGPoint(x: 405, y: 85),
+            CGPoint(x: 75, y: 235),
+            CGPoint(x: 185, y : 235),
+            CGPoint(x: 295, y: 235),
+            CGPoint(x: 405, y: 235)
+        ]
+        
+        // Load and unwrap the zener card images.
+        let circle = UIImage(named: "cardCircle")!
+        let cross = UIImage(named: "cardCross")!
+        let lines = UIImage(named: "cardLines")!
+        let square = UIImage(named: "cardSquare")!
+        let star = UIImage(named: "cardStar")!
+        
+        // Create an array of the images, one for each card, and shuffle them.
+        var images = [circle, circle, cross, cross, lines, lines, square, square, star, star]
+        images = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: images) as! [UIImage]
+        
+        for (index, position) in positions.enumerated() {
+            // Loop over each card position and create a new card view controller.
+            let card = CardViewController()
+            card.delegate = self
+            
+            // Use view controller containment to place one view inside another.
+            addChildViewController(card)
+            cardContainer.addSubview(card.view)
+            card.didMove(toParentViewController: self)
+            
+            // Position the card appropriately, then give it an image form the array.
+            card.view.center = position
+            card.front.image = images[index]
+            
+            if card.front.image == star {
+                card.isCorrect = true
+            }
+            
+            allCards.append(card)
+        }
+    }
+    
+    func cardTapped(_ tapped: CardViewController) {
+        guard view.isUserInteractionEnabled == true else { return }
+        view.isUserInteractionEnabled = false
+        
+        for card in allCards {
+            if card == tapped {
+                card.wasTapped()
+                card.perform(#selector(card.wasntTapped), with: nil, afterDelay: 1)
+            } else {
+                card.wasntTapped()
+            }
+        }
+        
+        perform(#selector(loadCards), with: nil, afterDelay: 2)
+    }
+    
+}
+
